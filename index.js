3bab2b6ea908cb3eda391926e5c195d15e50f9dc
shashikant verma <115453093+Shashikant1809@users.noreply.github.com>
2023-09-23 13:37:50 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let birthdayWish = [
+    "Wishing you a magical birthday!",
+    "May your special day be filled with happiness and love!",
+    "Here's to another year of amazing adventures!",
+    "May all your dreams and wishes come true on your birthday!",
+    "Sending you my warmest wishes on your special day!",
+    "Happy birthday to someone who brightens the world with their presence!"
+  
+  ]
+  let c = Math.floor(Math.random() * 6)
+  document.getElementById("birthdayWish").innerHTML = birthdayWish[c]
+  
+  let birthdayLine = [
+    "Enjoy your day to the fullest!",
+    "Have an unforgettable celebration!",
+    "Make beautiful memories that will last a lifetime!",
+    "Celebrate in style and have a blast!",
+    "May your birthday be as awesome as you are!",
+    "Cheers to another fantastic year!"
+  ]
+  
+  let d = Math.floor(Math.random() * 6)
+  document.getElementById("birthdayLine").innerHTML = birthdayLine[d]
+  
+  
